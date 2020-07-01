2e3b22b6920ac838ca1e842bcd99db79b9ce251a
Elvis Salvatierra <esalvati@pacifico.fin.ec>
2020-06-30 19:48:06 -0500

    listo

---
+++ especies-api/src/test/java/com/elvis/especiesapi/EspeciesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elvis.especiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspeciesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
