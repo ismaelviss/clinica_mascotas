96225567e8edfbc343ad863e3320560734ab7235
Elvis Salvatierra <esalvati@pacifico.fin.ec>
2020-06-30 19:47:08 -0500

    creacion de proyecto

---
+++ especies-api/src/main/java/com/elvis/especiesapi/EspeciesApiApplication.java
@@ -0,0 +1,13 @@
+package com.elvis.especiesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspeciesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EspeciesApiApplication.class, args);
+    }
+
+}
